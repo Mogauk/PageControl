c99e2ad0a9b404a642c52375c3cb21d0f9dec6e9
Alexey <line6@mail.ru>
2022-06-18 14:50:27 +0300

    Initial Commit

---
+++ PageControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by Alexey on 6/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
